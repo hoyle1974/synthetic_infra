5c1986771ca72b88eb030ede18fd05486a2678f6
Jack Strohm <hoyle.hoyle@gmail.com>
2023-02-04 23:18:34 +0000

    Prometheus working

--- prometheus.sh
+++ prometheus.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
+get_operator() {
+	if [ ! -d ./kube-prometheus-main ]
+	then
+		wget https://github.com/prometheus-operator/kube-prometheus/archive/main.zip
+		unzip main.zip
+		rm main.zip
+	else
+		echo "kube-prometheus-main already downloaded"
+	fi
+}
+
 up() {
-	helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-	helm repo update
-	helm install --create-namespace --namespace monitoring prom-operator-01 prometheus-community/kube-prometheus-stack 
+	get_operator
 
-	kubectl create -f prometheus-rbac.yaml
-	kubectl create -f prometheus.yaml
+	cd kube-prometheus-main
+	kubectl create -f manifests/setup
+	until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo "waiting"; done
+	kubectl create -f manifests/
+	cd ..
 
-	echo "Admin User: `kubectl get secret --namespace monitoring prom-operator-01-grafana -o jsonpath="{.data.admin-user}" | base64 --decode`"
-	echo "Password: `kubectl get secret --namespace monitoring prom-operator-01-grafana -o jsonpath="{.data.admin-password}" | base64 --decode`"
 
 	ip=`hostname -I | awk '{print $1}'`
 	echo "Prometheus expose: kubectl port-forward --address=$ip svc/prometheus-operated 9090:9090"
-	echo "Grafana expose: kubectl port-forward --address=$ip svc/prom-operator-01-grafana 8080:80"
+	echo "Grafana expose: kubectl port-forward --address=$ip svc/grafana 3000 -n monitoring"
+	echo "	Grafana user/pass is admin/admin"
 }
 
 down() {
-	kubectl delete -f prometheus.yaml
-	kubectl delete -f prometheus-rbac.yaml
-
-	helm uninstall prom-operator-01 
-
+	get_operator
+	cd kube-prometheus-main
+	kubectl delete --ignore-not-found=true -f manifests/ -f manifests/setup
+	cd ..
 }
 
 . ./base.sh $1
