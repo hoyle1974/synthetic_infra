ea98b3e8440f6cd696879f963e865a9e8c699328
Jack Strohm <hoyle.hoyle@gmail.com>
2023-02-16 19:49:43 +0000

    Defaults to debug

--- istio_logs.sh
+++ istio_logs.sh
@@ -2,5 +2,11 @@
 
 pod=`kubectl get pods -n istio-system -l app=istio-ingressgateway -o=jsonpath='{.items[0].metadata.name}'`
 
-istioctl proxy-config log {$pod}.istio-system --level debug
+if [[ "$1" = "debug" || -z "$1" ]]; then
+	level=debug
+else
+	level=$1
+fi
+
+istioctl proxy-config log ${pod}.istio-system --level $level
 kubectl -n istio-system logs $pod istio-proxy  -f
