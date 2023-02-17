1be59819b2f1d19077e58e1c1f8fdae812e25579
Jack Strohm <hoyle.hoyle@gmail.com>
2023-02-17 21:10:17 +0000

    Update gitea root url and expose via gateway

--- gitea.sh
+++ gitea.sh
@@ -5,15 +5,16 @@ up() {
 	helm repo add gitea-charts https://dl.gitea.io/charts/
 	helm repo update
 	kubectl create namespace gitea
+	kubectl label namespace gitea istio-injection=enabled
 	helm install gitea gitea-charts/gitea -f gitea-values.yaml -n gitea
-	kubectl create -f gitea-sm.yaml 
+	kubectl create -f gitea.yaml 
 
 	wait_for_pod gitea gitea-0
 }
 
 down() {
 	helm uninstall gitea -n gitea
-	kubectl delete -f gitea-sm.yaml 
+	kubectl delete -f gitea.yaml 
 	kubectl delete namespace gitea
 }
 
