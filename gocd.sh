133b3e210e48994ffbc4f2c0387f77ea6bb2f9b0
Jack Strohm <hoyle.hoyle@gmail.com>
2023-02-18 00:51:51 +0000

    Add gocd gateway entry

--- gocd.sh
+++ gocd.sh
@@ -5,12 +5,15 @@ up() {
 	helm repo update
 
 	kubectl create namespace gocd
+	kubectl label namespace gocd istio-injection=enabled
 	helm install gocd gocd/gocd --namespace gocd -f gocd-values.yaml
+	kubectl create -f gocd.yaml
 
 	wait_for_app gocd gocd
 }
 
 down() {
+	kubectl delete -f gocd.yaml
 	helm uninstall gocd -n gocd
 	kubectl delete namespace gocd
 }
