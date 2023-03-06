f62d15cc0c1b7fbcffeeaa1a716cbd198865250e
Jack Strohm <hoyle.hoyle@gmail.com>
2023-03-06 06:34:23 +0000

    CI/CD

--- scripts/pingpong_compile.sh
+++ scripts/pingpong_compile.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
+set -x
+
 container=$(buildah from docker.io/library/golang@sha256:d78cd58c598fa1f0c92046f61fde32d739781e036e3dc7ccf8fdb50129243dd8)
+echo "Container: $container"
 buildah copy $container "pingpong/*" .
 buildah config --env GOPATH="" $container
 buildah run $container go mod download
 buildah run $container go build .
 mountpoint=$(buildah mount $container)
-cp $mountpoint/pingpong ./bin
+echo "Mountpoint: $mountpoint"
+buildah run $container pwd
+buildah run $container find .
+cp $mountpoint/go/pingpong ./bin
 buildah unmount $mountpoint
 buildah rm $container
