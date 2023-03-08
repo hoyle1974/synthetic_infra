02ea9898baa52dc11f6de70f019a834ee8238587
Jack Strohm <hoyle.hoyle@gmail.com>
2023-03-07 21:30:03 -0800

    CI/CD

--- pingpong.sh
+++ pingpong.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 up() {
-	docker build --tag ping . -f ./Dockerfile.ping 
-	docker build --tag pong . -f ./Dockerfile.pong 
-	docker tag ping:latest k3d-myregistry.localhost:12345/ping:v1
-	docker tag pong:latest k3d-myregistry.localhost:12345/pong:v1
-	docker push k3d-myregistry.localhost:12345/ping:v1
-	docker push k3d-myregistry.localhost:12345/pong:v1
+	#docker build --tag ping . -f ./Dockerfile.ping 
+	#docker build --tag pong . -f ./Dockerfile.pong 
+	#docker tag ping:latest k3d-myregistry.localhost:12345/ping:v1
+	#docker tag pong:latest k3d-myregistry.localhost:12345/pong:v1
+	#docker push k3d-myregistry.localhost:12345/ping:v1
+	#docker push k3d-myregistry.localhost:12345/pong:v1
 	kubectl create -f ping.yaml
 	kubectl create -f pong.yaml
 }
